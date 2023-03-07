3e84d5b931f1b7706175f218b2562be10b32c874
James Liu <52677895+jamesliu4c@users.noreply.github.com>
2023-03-07 22:57:46 +0000

    Add some hints

--- README.md
+++ README.md
@@ -13,10 +13,16 @@ pip-compile
 pip-sync
 ```
 
-Once that is set up, you can run the app using simply:
+Once that is set up, you can run the app with hot reload:
 
 ```bash
-flask run
+flask --debug run
 ```
 
-It will tell you an address with a port forward. So now you have a working API. Then it is up to you. Have a fun interview.
+Use httpie to make requests from the terminal easy
+
+```bash
+http :5000/campaign/ start_date=="2023-01-01" end_date=="2023-01-31"
+```
+
+It will tell you an address with a port forward. So now you have a working API. The port forward by default only works in your (interviewer) local. Click on the PORTS tab (on the bottom toolbox, next to TERMINAL), right click on the forwarded port, and select public. Note, it becomes private every time the server restarts, so keep an eye on that tab and re-publicize the port when you need. Then it is up to you. Have a fun interview.
--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import requests
 from .config import Config
 
 app = Flask(__name__)
@@ -11,3 +12,27 @@ app.config.from_object(Config)
 def root():
     """The root route"""
     return jsonify({"status": "ok"})
+
+
+@app.route("/campaign/")
+def campaign():
+    """Gives campaigns, a flattened list of objects. Each object is a campaign,
+    which should have all of the entries from the partner campaign endpoint and
+    the partner campaign stats endpoint.
+    
+    Like:
+
+    >>> campaign("2023-01-01", "2023-01-31")
+    {
+        "campaigns": [
+            {
+                "id": 0, type: "", "name": "", "date_start": "", ...
+                "impressions": 0, "spend": 0, "unique_users": 0, ...
+            },
+            ...
+        ]
+    }
+    """
+    print(request.args)
+
+    return jsonify({"error": "Not implemented"}), 500
