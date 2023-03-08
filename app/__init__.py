e4a92df751d615b443fd60f2d753ccd6525c3c51
James Liu <52677895+jamesliu4c@users.noreply.github.com>
2023-03-08 00:56:11 +0000

    Add example advertisers route

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,8 @@ from .config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
 
+API_ROOT = "https://winter-dawn-3615.fly.dev"
 
 @app.route("/")
 def root():
@@ -14,9 +13,27 @@ def root():
     return jsonify({"status": "ok"})
 
 
-@app.route("/campaign/")
-def campaign():
-    """Gives campaigns, a flattened list of objects. Each object is a campaign,
+@app.route("/advertiser/")
+def advertiser():
+    """Gets the advertiser information from the partner API
+    """
+    if not request.args.get("id"):
+        return jsonify({"error": "Must supply query for id"}), 404
+    advertiser_id = request.args["id"]
+
+    resp = requests.get(f"{API_ROOT}/advertiser/{advertiser_id}/")
+
+    if not resp.ok:
+        return jsonify({"error": resp.reason}), resp.status_code
+    
+    advertiser = resp.json()
+    return jsonify({"advertiser": advertiser})
+
+
+
+@app.route("/campaigns/")
+def campaigns():
+    """Gives campaigns, a list of flattened objects. Each object is a campaign,
     which should have all of the entries from the partner campaign endpoint and
     the partner campaign stats endpoint.
     
@@ -33,6 +50,4 @@ def campaign():
         ]
     }
     """
-    print(request.args)
-
-    return jsonify({"error": "Not implemented"}), 500
+    return jsonify({"error": "Not implemented"}), 501
